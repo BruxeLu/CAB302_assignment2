db1ff06b422822a05ae19a71dc30d3c2c60016f2
howardtaylor <howard.taylor@student.qut.edu.au>
2016-05-10 15:04:49 +1000

    add test file

---
+++ AircraftSimulator/src/asgn2Tests/FirstTests.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Howard
+ *
+ */
+public class FirstTests {
+
+	/**
+	 * Test method for {@link asgn2Passengers.First#noSeatsMsg()}.
+	 */
+	@Test
+	public void testNoSeatsMsg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link asgn2Passengers.First#upgrade()}.
+	 */
+	@Test
+	public void testUpgrade() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link asgn2Passengers.First#First(int, int)}.
+	 */
+	@Test
+	public void testFirstIntInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link asgn2Passengers.First#First()}.
+	 */
+	@Test
+	public void testFirst() {
+		fail("Not yet implemented");
+	}
+
+}
